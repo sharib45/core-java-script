17443668f7407e274e46905d3c98709d2ede7836
sharib45 <sharibshahid45@gmail.com>
2023-08-27 08:12:38 +0000

    done

--- 01_basic/01_Variable.js
+++ 01_basic/01_Variable.js
@@ -2,5 +2,18 @@ const accountId = 123456
 let = acountEmail = "sharib@gmail.com"
 var accountPassword = "123456789"
 City = "jaipur"
+let accountstate;
 
-accountId=456
+//accountId=456
+
+// account id cant not be change beacuse account id variable is const
+
+acountEmail="sharib@gmail.com"
+
+accountPassword="hello"
+city="delhi"
+
+
+console.log(accountId);
+
+console.table([accountId,accountPassword,acountEmail,city,accountstate])
