1584fed67229a1371bbbca398b5d9084ab62e963
sharib45 <sharibshahid45@gmail.com>
2023-08-27 12:08:12 +0000

    done

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// alert(3+3)  we are using node js not browser
+
+console.log(3+3)
+
+let name = "sharib"
+let age = 18
+let logIn =true
+
+
+// primative data type
+
+// number => range 2 to power 53
+//bigint 
+//string => ""
+//bolean => false
+//null => standlone value
+//undefined => nothing value ASSIGNED
+//symbol => unique 
+
+
+//Non primnitve data type
+
+// 1 objet
+
+
+console.table([typeof "sharib",typeof age,typeof null,typeof undefined])
+
