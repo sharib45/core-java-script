f34f8823b0b7cda97cb2290255601f3ba0ef130c
sharib45 <sharibshahid45@gmail.com>
2023-09-02 15:00:37 +0000

    done

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,46 @@
+
+/*
+//String to number
+
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+*/
+/*
+
+null to number
+
+let score = null
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+*/
+
+/*
+
+undefind to number
+
+let score = undefined
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+*/
